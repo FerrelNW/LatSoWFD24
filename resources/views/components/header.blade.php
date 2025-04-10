07dd47acb711ed423d02f08189593efdb4a5c77e
FerrelNW <ferrel.nw@gmail.com>
2025-04-10 20:58:14 +0700

    abu abu

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,3 @@
-<header class="bg-red-600 text-center text-white p-4">
-    <h1 class="text-xl font-bold">Cinema Ticket Booking</h1>
+<header class="bg-gray-800 text-center text-white p-4">
+    <a href="/"><h1 class="text-xl font-bold">Cinema Ticket Booking</h1></a>
 </header>
